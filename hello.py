57dbed41be9cfc2e2d20db1c9f252cc1bcb0f182
zamemirs <112723194+zamemirs@users.noreply.github.com>
2023-10-25 17:39:52 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
    return "<h1>Hello Azure!</h1>"
+
+if __name__ == "__main__":
+    # Run the Flask app on port 80 (or any other port you prefer)
+    app.run(host='0.0.0.0', port=80)
